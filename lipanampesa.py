62d5b91ad58e268d7afafe95a02598af37194ded
hasalioma2 <56221788+hasalioma2@users.noreply.github.com>
2020-08-18 13:56:11 +0300

    keys

--- lipanampesa.py
+++ lipanampesa.py
@@ -1,1 +1,31 @@
-print ("Welcome")
+import requests
+
+from datetime import datetime
+import keys
+print (datetime.now())
+
+# 2020-08-18 13:50:57.415621
+
+def lipa_na_mpesa():
+    access_token = "Access-Token"
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = { "Authorization": "Bearer %s" % access_token }
+    request = {
+    "BusinessShortCode": keys.business_shortCode,
+    "Password": " ",
+    "Timestamp": " ",
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": "1",
+    "PartyA": keys.PhoneNumber,
+    "PartyB": keys.business_shortCode,
+    "PhoneNumber": keys.PhoneNumber,
+    "CallBackURL": "https://fullstackdjango.com/callback",
+    "AccountReference": "1234567",
+    "TransactionDesc": "test Mpesa"
+    }
+
+    response = requests.post(api_url, json = request, headers=headers)
+
+    print (response.text)
+
+# lipa_na_mpesa()
